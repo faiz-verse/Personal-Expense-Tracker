95a893463825254b626a4c7fc0dff46ac217796e
faiz-verse <faizzzverseee@gmail.com>
2025-05-28 14:21:07 +0530

    Added project files

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+import Button from './components/Button';
+import './components/Button.css'
+
+function App() {
+
+    const [value, setValue] = useState(1)
+
+    return (
+        <div id='App'>
+            <div class>
+                <span style={{ fontWeight: 800, fontSize: "18px" }}>{value}</span>
+                <Button onClick={() => setValue(value + 1)} text="Click Me"></Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
