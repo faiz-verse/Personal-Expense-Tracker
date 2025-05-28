e632d36787e1e5c877bc863d42b6e91323ccbeb1
faiz-verse <faizzzverseee@gmail.com>
2025-05-28 17:57:49 +0530

    added pages for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+// For Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Button from './components/Button';
-import './components/Button.css'
+import "./App.css";
+
+// Importing Pages
+import Home from "./pages/Home";
+import "./pages/Home.css";
+import Dashboard from "./pages/Dashboard";
+import "./pages/Dashboard.css";
+
+import Button from "./components/Button";
+import "./components/Button.css";
 
 function App() {
-
-    const [value, setValue] = useState(1)
+    const [value, setValue] = useState(1);
 
     return (
-        <div id='App'>
+        <div id="App">
             <div>
                 <span style={{ fontWeight: 800, fontSize: "18px" }}>{value}</span>
                 <Button onClick={() => setValue(value + 1)} text="Click Me"></Button>
             </div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/dashboard" element={<Dashboard />}></Route>
+                </Routes>
+            </Router>
         </div>
     );
 }
