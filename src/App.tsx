de57c659886499be366e2c523aba0cdc2a0c2dce
faiz-verse <faizzzverseee@gmail.com>
2025-05-29 17:43:54 +0530

    Designed the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,12 @@ import "./App.css";
 
 // Importing Pages
 import Home from "./pages/Home";
-import "./pages/Home.css";
 import Dashboard from "./pages/Dashboard";
-import "./pages/Dashboard.css";
-
-import Button from "./components/Button";
-import "./components/Button.css";
 
 function App() {
-    const [value, setValue] = useState(1);
 
     return (
         <div id="App">
-            <div>
-                <span style={{ fontWeight: 800, fontSize: "18px" }}>{value}</span>
-                <Button onClick={() => setValue(value + 1)} text="Click Me"></Button>
-            </div>
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
