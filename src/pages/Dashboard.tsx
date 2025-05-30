7dd09c70b0331f7b7f16b28ead885e9dff9de87e
faiz-verse <faizzzverseee@gmail.com>
2025-05-30 11:24:45 +0530

    did more styling and added react-icons in dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,31 +1,49 @@
 import React, { useState } from "react";
 
-// for lottie animation
-// import Lottie from "@lottielab/lottie-player/react";
+import { IconBaseProps } from "react-icons";
+import { BsFillHouseDoorFill, BsFileEarmarkBarGraphFill, BsPiggyBankFill, BsArrowLeftRight , BsArrowBarLeft} from "react-icons/bs";
+
 // import { useNavigate } from "react-router-dom";
 
 import "./Dashboard.css";
 
 const Dashboard = () => {
+
+    const DashboardIcon = BsFillHouseDoorFill as React.ComponentType<IconBaseProps>;
+    const ReportIcon = BsFileEarmarkBarGraphFill as React.ComponentType<IconBaseProps>;
+    const BudgetIcon = BsPiggyBankFill as React.ComponentType<IconBaseProps>;
+    const HistoryIcon = BsArrowLeftRight as React.ComponentType<IconBaseProps>;
+    const BackIcon = BsArrowBarLeft as React.ComponentType<IconBaseProps>;
+
     return (
         <div id="Dashboard">
-            <div id="side menu">
+            <div id="side-menu">
                 <div id="side-menu-title">ExTrack</div>
                 <div id="menu-options">
                     <div className="menu-option" id="dashboard-option">
-                        Dashboard
+                        <DashboardIcon className="menu-option-icon" size={20} color='#4d69ff'
+                            style={{background: 'white'}}
+                        />Dashboard
                     </div>
                     <div className="menu-option" id="resports-option">
-                        Reports
+                        <ReportIcon className="menu-option-icon" size={20} color='#4d69ff'
+                            style={{background: 'white'}}
+                        />Reports
                     </div>
                     <div className="menu-option" id="budgets-option">
-                        Budgets
+                        <BudgetIcon className="menu-option-icon" size={20} color='#4d69ff'
+                            style={{background: 'white'}}
+                        />Budgets
                     </div>
                     <div className="menu-option" id="profile-option">
-                        Transaction History
+                        <HistoryIcon className="menu-option-icon" size={20} strokeWidth={0.5} color='#4d69ff'
+                            style={{background: 'white'}}
+                        />Transaction History
                     </div>
                 </div>
-                <div id="menu-footer">User Name</div>
+                <div id="menu-footer">
+                    <BackIcon className="back-icon" size={20} strokeWidth={0.5} color='#4d69ff'/> User Name
+                </div>
             </div>
         </div>
     );
