e539a0535b5a3405480ee5e8ff7e23843bb34610
faiz-verse <faizzzverseee@gmail.com>
2025-06-05 16:35:57 +0530

    added the budget modal

---
+++ src/components/AddBudgetModal.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import './AddBudgetModal.css'
+
+interface Props{
+    isModalActive: boolean,
+    setIsModalActive: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const AddBudgetModal = ({isModalActive, setIsModalActive}: Props) => {
+  return (
+    <div id='add-budget-overlay' style={{display: isModalActive? 'flex': 'hidden'}}>
+            <div id="add-budget-modal">
+                <h2>Add a New Budget</h2>
+
+                <form>
+
+                    <label>
+                        Budget:
+                        <select>
+                            <option>Main</option>
+                            <option>Budget 1</option>
+                            <option>Budget 2</option>
+                            <option>Budget 3</option>
+                            <option>Budget 4</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Title:
+                        <input type="text" placeholder="e.g. Grocery shopping" />
+                    </label>
+
+                    <label>
+                        Amount (₹):
+                        <input type="number" placeholder="e.g. 1500" />
+                    </label>
+
+                    <label>
+                        Category:
+                        <select>
+                            <option>Food</option>
+                            <option>Transport</option>
+                            <option>Rent</option>
+                            <option>Entertainment</option>
+                            <option>Others</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Date:
+                        <input type="date" />
+                    </label>
+
+                    <label>
+                        Description:
+                        <textarea placeholder="Add optional notes..." />
+                    </label>
+
+                    <div className="modal-buttons">
+                        <button type="submit">Add Expense</button>
+                        <button type="button" onClick={()=> setIsModalActive(!isModalActive)}>Cancel</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+  )
+}
+
+export default AddBudgetModal
