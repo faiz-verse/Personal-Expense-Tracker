680c80b1a9dd80c5f6837fce62731185c82ee005
faiz-verse <faizzzverseee@gmail.com>
2025-06-06 17:03:17 +0530

    made few more changes

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -3,14 +3,31 @@ import React from 'react'
 import './History.css'
 
 const History = () => {
-  return (
-    <div id='history'>
-        <div id='title'>
+    return (
+        <div id='history'>
+            <div id='title'>
                 <span>Transaction History</span>
                 <span>History of all your expenses 💸</span>
+            </div>
+
+            <div id='all-expenses'>
+                <span>Your Expenses</span>
+                <div id='expenses'>
+                    <div id='expense-head'></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                    <div className="expense"></div>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default History
